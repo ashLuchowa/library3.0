ed56eef36781c0c61aa04d08ae683fd0646b3018
ashLuchowa <ashwinluchowa@gmail.com>
2025-01-10 16:48:27 +0800

    Create Objects

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// Default Book Items
+let harryPotter = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, true)
+
+// Book Array Library
+const myLibrary = [harryPotter];
+
+// Display Books UI
+
+// Book Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+// Push each books to the library
+function addBookToLibrary() {
+
+};
