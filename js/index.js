021e1187e205cd76c777ecaccbdcb19e916975a7
ashLuchowa <ashwinluchowa@gmail.com>
2025-01-10 17:02:00 +0800

    Construct loop through Array function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // Default Book Items
 let harryPotter = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, true)
+let pridePrejudice = new Book("Pride and Prejudice", "	Jane Austen", 345, true)
 
 // Book Array Library
-const myLibrary = [harryPotter];
+const myLibrary = [harryPotter, pridePrejudice];
 
 // Display Books UI
 
@@ -16,5 +17,9 @@ function Book(title, author, pages, read) {
 
 // Push each books to the library
 function addBookToLibrary() {
-
+    for(let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+    }
 };
+
+addBookToLibrary();
